e7fcaf919212214d3a5343aaa3218deb336a12a4
Jason McClurg <mcclujdd@gmail.com>
2020-03-01 18:51:22 -0500

    creature objects created

--- htcw_mar/js/principle.js
+++ htcw_mar/js/principle.js
@@ -1,39 +1,65 @@
-var creatureType = [
-  "Witch",
-  "Dragon",
-  "Snake",
-  "River Troll"
-];
 
-var minStats = [
-  30,
-  50,
-  60,
-  80
-];
-var maxStats =[
-  60,
-  90,
-  92
-];
+var witch = {
+  type:"Witch",
+  strMin:60,
+  strMax:80,
+  hlthMin:50,
+  hlthMax:60,
+  health: function(){
+    this.hlthMax= Math.floor(this.hlthMax)
+    this.hlthMin= Math.ceil(this.hlthMin)
+    return Math.floor(Math.random() * (this.hlthMax-this.hlthMin)+this.hlthMin)},
+  strength:function(){
+    this.strMax= Math.floor(this.strMax)
+    this.strMin= Math.ceil(this.strMin)
+    return Math.floor(Math.random() * (this.strMax-this.strMin)+this.strMin)}
+  };
 
-/*initial health and strength values generate
-randomly within min/max parameters based on creature type.*/
-var minS;
-var minH;
-
-function healthGen(min, max){
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max-min)+min);
+var dragon = {
+  type:"Dragon",
+  strMin:80,
+  strMax:90,
+  hlthMin:80,
+  hlthMax:90,
+  health: function(){
+    this.hlthMax= Math.floor(this.hlthMax)
+    this.hlthMin= Math.ceil(this.hlthMin)
+    return Math.floor(Math.random() * (this.hlthMax-this.hlthMin)+this.hlthMin)},
+  strength:function(){
+    this.strMax= Math.floor(this.strMax)
+    this.strMin= Math.ceil(this.strMin)
+    return Math.floor(Math.random() * (this.strMax-this.strMin)+this.strMin)}
+};
+var snake = {
+  type:"Snake",
+  strMin:30,
+  strMax:60,
+  hlthMin:30,
+  hlthMax:90,
+  health: function(){
+    this.hlthMax= Math.floor(this.hlthMax)
+    this.hlthMin= Math.ceil(this.hlthMin)
+    return Math.floor(Math.random() * (this.hlthMax-this.hlthMin)+this.hlthMin)},
+  strength:function(){
+    this.strMax= Math.floor(this.strMax)
+    this.strMin= Math.ceil(this.strMin)
+    return Math.floor(Math.random() * (this.strMax-this.strMin)+this.strMin)}
+};
+var riverTroll = {
+  type:"River Troll",
+  strMin:22,
+  strMax:65,
+  hlthMin:60,
+  hlthMax:92,
+  health: function(){
+    this.hlthMax= Math.floor(this.hlthMax)
+    this.hlthMin= Math.ceil(this.hlthMin)
+    return Math.floor(Math.random() * (this.hlthMax-this.hlthMin)+this.hlthMin)},
+  strength:function(){
+    this.strMax= Math.floor(this.strMax)
+    this.strMin= Math.ceil(this.strMin)
+    return Math.floor(Math.random() * (this.strMax-this.strMin)+this.strMin)}
 };
 
-function strGen(min, max){
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max-min)+min);
-};
 
-if (creatureType.indexOf(0)) {
-  return maxStats.0
-}
+console.log(witch.strength());
